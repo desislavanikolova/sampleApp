b239291f182620764ba61d8befeec507c2f9868e
Desislava Nikolova <desi.niko@gmail.com>
2013-03-31 19:24:02 +0300

    Add SSL in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 SampleApp2::Application.configure do
-  # Settings specified here will take precedence over those in config/application.rb
+# Settings specified here will take precedence over those in config/application.rb
 
-  # Code is not reloaded between requests
+# Code is not reloaded between requests
   config.cache_classes = true
 
   # Full error reports are disabled and caching is turned on
@@ -28,7 +28,7 @@ SampleApp2::Application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
+  config.force_ssl = true
 
   # See everything in the log (default is :info)
   # config.log_level = :debug
@@ -61,7 +61,7 @@ SampleApp2::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
-  # Log the query plan for queries taking more than this (works
-  # with SQLite, MySQL, and PostgreSQL)
-  # config.active_record.auto_explain_threshold_in_seconds = 0.5
+# Log the query plan for queries taking more than this (works
+# with SQLite, MySQL, and PostgreSQL)
+# config.active_record.auto_explain_threshold_in_seconds = 0.5
 end
